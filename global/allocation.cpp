4f94f22c775f067917325078d9da910c77b51005
Соснев Леонид Михайлович <lsosniev@dolyadomain.com>
2019-06-04 17:41:26 +0300

    feat(allocation): add pre- and post- callback setters...
    the set callbacks are invoked immediately before and after
    the (de)allocation itself

--- global/allocation.cpp
+++ global/allocation.cpp
@@ -5,17 +5,62 @@
 
 using namespace std;
 
-void deallocate_nothrow(void * ptr)
-{
-    std::free(ptr);
+void empty_s(size_t size) {}
+void empty_p(const void * ptr) {}
+void empty_ps(const void * ptr, size_t size) {}
+
+static void (*pre_dealloc_callback)(const void * ptr) = empty_p;
+static void (*pre_dealloc_size_callback)(const void * ptr, size_t size) = empty_ps;
+static void (*pre_alloc_callback)(size_t size) = empty_s;
+
+static void (*post_dealloc_callback)(const void * ptr) = empty_p;
+static void (*post_dealloc_size_callback)(const void * ptr, size_t size) = empty_ps;
+static void (*post_alloc_callback)(const void * ptr, size_t size) = empty_ps;
+
+
+void _utl::setPreDeallocationCallbacks(
+    void(*pointerOnly)(const void * ptr),
+    void(*pointerAndSize)(const void * ptr, size_t size)
+){
+    if (!pointerAndSize) { std::abort(); }
+    if (!pointerOnly) { std::abort(); }
+    pre_dealloc_callback = pointerOnly;
+    pre_dealloc_size_callback = pointerAndSize;
 }
-void deallocate_nothrow(void * ptr, size_t size)
-{
-    std::free(ptr);
+void _utl::setPostDeallocationCallbacks(
+    void(*pointerOnly)(const void * ptr),
+    void(*pointerAndSize)(const void * ptr, size_t size)
+){
+    if (!pointerAndSize) { std::abort(); }
+    if (!pointerOnly) { std::abort(); }
+    post_dealloc_callback = pointerOnly;
+    post_dealloc_size_callback = pointerAndSize;
 }
-void * allocate_nothrow(size_t size)
-{
+void _utl::setPreAllocationCallback(void(*sizeOnly)(size_t size)) {
+    if (!sizeOnly) { std::abort(); }
+    pre_alloc_callback = sizeOnly;
+}
+void _utl::setPostAllocationCallback(void(*pointerAndSize)(const void * ptr, size_t size)) {
+    if (!pointerAndSize) { std::abort(); }
+    post_alloc_callback = pointerAndSize;
+}
+
+
+
+void deallocate_nothrow(void * ptr) {
+    pre_dealloc_callback(ptr);
+    std::free(ptr);
+    post_dealloc_callback(ptr);
+}
+void deallocate_nothrow(void * ptr, size_t size) {
+    pre_dealloc_size_callback(ptr,size);
+    std::free(ptr);
+    post_dealloc_size_callback(ptr,size);
+}
+void * allocate_nothrow(size_t size) {
+    pre_alloc_callback(size);
     void * ptr = std::malloc(size);
+    post_alloc_callback(ptr,size);
     return ptr;
 }
 void * allocate(size_t size)
--- global/allocation.hpp
+++ global/allocation.hpp
@@ -2,3 +2,23 @@
 
 #include <stdexcept>
 #include <cstdint>
+
+namespace _utl
+{
+
+    void setPreDeallocationCallbacks(
+        void(*)(const void * ptr),
+        void(*)(const void * ptr, size_t size)
+    );
+    void setPostDeallocationCallbacks(
+        void(*)(const void * ptr),
+        void(*)(const void * ptr, size_t size)
+    );
+    void setPreAllocationCallback(
+        void(*)(size_t size)
+    );
+    void setPostAllocationCallback(
+        void(*)(const void * ptr, size_t size)
+    );
+
+}
