796dd79d463a3be4c5129cb3e6f0269ca8565920
Sosniev Leonid <lsosniev@dolyadomain.com>
2020-01-03 09:05:52 +0200

    feat(io/{Reader,Writer}.hpp): add dummy reader and writer (methods do nothing)

--- io/Reader.hpp
+++ io/Reader.hpp
@@ -10,4 +10,16 @@ namespace _utl
         virtual ~AbstractReader() {}
     };
 
+    class DummyReader : public AbstractReader {
+    private:
+        uint32_t m_reportCount;
+    public:
+        DummyReader(bool reportZeroBytes) : m_reportCount(reportZeroBytes ? 0 : uint32_t(-1))
+        {}
+        virtual uint32_t read(void * data, uint32_t maxSize) override {
+            (void) data;
+            return (maxSize < m_reportCount) ? maxSize : m_reportCount;
+        }
+    };
+
 }
--- io/Writer.hpp
+++ io/Writer.hpp
@@ -11,4 +11,10 @@ namespace _utl
         virtual ~AbstractWriter() {}
     };
 
+    class DummyWriter : public AbstractWriter {
+    public:
+        virtual void write(const void * data, uint32_t size) override { (void) data; (void) size; }
+        virtual void flush() override {}
+    };
+
 }
