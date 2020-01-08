577fc44496583866c7ccf4aff7ceb027a4dac59d
Sosniev Leonid <lsosniev@dolyadomain.com>
2020-01-08 11:33:06 +0200

    fix(io/Writer.hpp): writer should report successfully written bytes count

--- io/Writer.hpp
+++ io/Writer.hpp
@@ -6,21 +6,21 @@ namespace _utl
 
     class AbstractWriter {
     public:
-        virtual void write(const void * data, uint32_t size) = 0;
-        virtual void flush() = 0;
+        virtual uint32_t write(const void * data, uint32_t size) = 0;
+        virtual bool flush() = 0;
         virtual ~AbstractWriter() {}
     };
 
     class DummyWriter : public AbstractWriter {
     public:
-        virtual void write(const void * data, uint32_t size) override { (void) data; (void) size; }
-        virtual void flush() override {}
+        virtual uint32_t write(const void * data, uint32_t size) override { (void) data; (void) size; return size; }
+        virtual bool flush() override { return true; }
     };
 
     class NullWriter : public AbstractWriter {
     public:
-        void write(const void * data, uint32_t size) override { std::abort(); (void) data; (void) size; }
-        void flush() override { std::abort(); }
+        uint32_t write(const void * data, uint32_t size) override { std::abort(); (void) data; (void) size; return 0; }
+        bool flush() override { std::abort(); return false; }
     };
 
 }
