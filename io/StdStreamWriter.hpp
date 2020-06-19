d5e8e141da36d0fa0ab65364768f4bd003a88d95
leonid <leonid.sosniev@gmail.com>
2020-06-19 17:16:26 +0300

    refac - +io/tests/ - correcting StdStreamWriter class to get tests pass

--- io/StdStreamWriter.hpp
+++ io/StdStreamWriter.hpp
@@ -2,6 +2,8 @@
 
 #include <utl/io/Writer.hpp>
 #include <iostream>
+#include <exception>
+#include <cassert>
 
 namespace _utl
 {
@@ -9,18 +11,26 @@ namespace _utl
     private:
         std::ostream * m_stream;
     public:
-        StdStreamWriter(std::ostream * stream = &std::cerr) : m_stream(stream)
-        {}
+        StdStreamWriter(std::ostream * stream = &std::cerr) : m_stream(stream) {
+            if (!stream) {
+                throw std::invalid_argument{"stream is null"};
+            }
+        }
         ~StdStreamWriter() override;
 
         uint32_t write(const void * data, uint32_t size) override
         {
+            assert(m_stream);
+            if (!size) { return 0; }
+            if (!data) { throw std::invalid_argument{"cannot write null data"}; }
+
             auto posBefore = m_stream->tellp();
-            m_stream->write(static_cast<const char*>(data), static_cast<int>(size)).tellp();
+            m_stream->write(static_cast<const char*>(data), static_cast<std::streamsize>(size)).tellp();
             auto posAfter = m_stream->tellp();
             return posAfter - posBefore;
         }
         bool flush() override {
+            assert(m_stream);
             m_stream->flush();
             return !(m_stream->failbit || m_stream->badbit);
         }
