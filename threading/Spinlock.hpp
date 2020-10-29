c623ec9d4cd2762f45b4a6ef36055d42358261b2
leonid <leonid.sosniev@gmail.com>
2020-10-29 12:41:04 +0300

    refac - threading/Spinlock.{-cpp,~hpp} - move definitions from source file to header (make this header-only)

--- threading/Spinlock.hpp
+++ threading/Spinlock.hpp
@@ -2,20 +2,55 @@
 
 #include <chrono>
 #include <atomic>
+#include <thread>
 
 namespace _utl
 {
 
-    class SpinLock {
+    namespace details { namespace SpinLock {
+
+        template<bool enable = true> struct OptionalSleep {
+            static void go() {
+                std::this_thread::sleep_for(std::chrono::milliseconds(0));
+            }
+        };
+        template<> struct OptionalSleep<false> {
+            static void go() {}
+        };
+
+    }}
+
+    template<bool useSleeping = false> class SpinLock {
     public:
-        SpinLock(bool useSleeping = false);
-        void lock();
-        bool try_lock();
-        bool try_lock_for(std::chrono::milliseconds);
-        void unlock();
+        SpinLock()
+        {}
+        void lock()
+        {
+            while (lck.test_and_set(std::memory_order_acquire)) {
+                details::SpinLock::OptionalSleep<useSleeping>::go();
+            }
+        }
+        bool try_lock()
+        {
+            return !lck.test_and_set(std::memory_order_acquire);
+        }
+        bool try_lock_for(std::chrono::milliseconds msec)
+        {
+            auto deadline = std::chrono::steady_clock::now() + msec;
+            while (std::chrono::steady_clock::now() < deadline) {
+                if (try_lock() == true) {
+                    return true;
+                }
+                else details::SpinLock::OptionalSleep<useSleeping>::go();
+            }
+            return false;
+        }
+        void unlock()
+        {
+            lck.clear(std::memory_order_release);
+        }
     private:
         volatile std::atomic_flag lck = ATOMIC_FLAG_INIT;
-        const bool useSleeping;
     };
 
 }
