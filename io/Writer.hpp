e41afe7e3fd1e6c721c3bc51cec0301a597f79bc
leonid <leonid.sosniev@gmail.com>
2022-07-11 15:55:22 +0300

    style: remove unused parameter names

--- io/Writer.hpp
+++ io/Writer.hpp
@@ -25,13 +25,13 @@ namespace _utl
 
     class DummyWriter : public AbstractWriter {
     public:
-        virtual uint32_t write(const void * data, uint32_t size) final override { (void) data; (void) size; return size; }
+        virtual uint32_t write(const void *, uint32_t size) final override { return size; }
         virtual bool flush() final override { return true; }
     };
 
     class NullWriter : public AbstractWriter {
     public:
-        virtual uint32_t write(const void *, uint32_t) final override { std::abort(); (void) data; (void) size; return 0; }
+        virtual uint32_t write(const void *, uint32_t) final override { std::abort(); return 0; }
         virtual bool flush() final override { std::abort(); return false; }
     };
 
