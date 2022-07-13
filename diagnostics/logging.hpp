db4fab095eb943e6db49af30d0a7414d0ed05280
leonid <leonid.sosniev@gmail.com>
2022-07-14 01:59:10 +0300

    refac(Arg,+args.hpp): move Arg and part of logEvent() implementation to a separate header

--- diagnostics/logging.hpp
+++ diagnostics/logging.hpp
@@ -16,6 +16,7 @@
 #include <thread>
 #include <unordered_map>
 
+#include <utl/diagnostics/logging/args.hpp>
 #include <utl/io/Writer.hpp>
 #include <utl/io/Reader.hpp>
 
@@ -36,73 +37,6 @@ namespace _utl { namespace logging {
             return { text, *text ? create(text+1).end : text};
         }
     };
-
-    struct Arg {
-        union Value {
-            int8_t    i8;   uint8_t   u8;
-            int16_t   i16;  uint16_t  u16;
-            int32_t   i32;  uint32_t  u32;
-            int64_t   i64;  uint64_t  u64;
-            float     f32;  double    f64;
-            char      Char;
-            ThreadId  Thread;
-            TimePoint EpochNsec;
-            const void * ArrayPointer;
-        };
-        enum class TypeID : uint8_t {
-            TI_NONE      = 0,  __ISARRAY            = 1,
-            TI_u8        = 2,  TI_arrayof_u8        = 3,
-            TI_u16       = 4,  TI_arrayof_u16       = 5,
-            TI_u32       = 6,  TI_arrayof_u32       = 7,
-            TI_u64       = 8,  TI_arrayof_u64       = 9,
-            TI_i8        = 10, TI_arrayof_i8        = 11,
-            TI_i16       = 12, TI_arrayof_i16       = 13,
-            TI_i32       = 14, TI_arrayof_i32       = 15,
-            TI_i64       = 16, TI_arrayof_i64       = 17,
-            TI_f32       = 18, TI_arrayof_f32       = 19,
-            TI_f64       = 20, TI_arrayof_f64       = 21,
-            TI_Char      = 22, TI_arrayof_Char      = 23,
-            TI_Thread    = 24, TI_arrayof_Thread    = 25,
-            TI_EpochNsec = 26, TI_arrayof_EpochNsec = 27,
-            TI_UNKNOWN   = 28,
-            __TYPE_ID_COUNT
-        };
-        static uint8_t typeSize(TypeID type) {
-            static const uint8_t typeId2Size[(int) TypeID::__TYPE_ID_COUNT] = {
-                0, 0, // TI_NONE
-                sizeof(uint8_t)  , sizeof(uint8_t),
-                sizeof(uint16_t) , sizeof(uint16_t),
-                sizeof(uint32_t) , sizeof(uint32_t),
-                sizeof(uint64_t) , sizeof(uint64_t),
-                sizeof(int8_t)   , sizeof(int8_t),
-                sizeof(int16_t)  , sizeof(int16_t),
-                sizeof(int32_t)  , sizeof(int32_t),
-                sizeof(int64_t)  , sizeof(int64_t),
-                sizeof(float)    , sizeof(float),
-                sizeof(double)   , sizeof(double),
-                sizeof(char)     , sizeof(char),
-                sizeof(ThreadId), sizeof(ThreadId),
-                sizeof(TimePoint), sizeof(TimePoint),
-            };
-            if (unsigned(type) >= (unsigned) TypeID::__TYPE_ID_COUNT) {
-                throw std::runtime_error{ "Arg::typeSize() was given an invalid type ID" };
-            }
-            return typeId2Size[(unsigned) type];
-        }
-        Value valueOrArray;
-        TypeID type;
-        uint32_t arrayLength;
-    };
-    inline Arg::TypeID operator|(Arg::TypeID a, Arg::TypeID b)
-    {
-        assert(a == Arg::TypeID::__ISARRAY || b == Arg::TypeID::__ISARRAY);
-        return static_cast<Arg::TypeID>(int(a) | int(b));
-    }
-    inline Arg::TypeID operator&(Arg::TypeID a, Arg::TypeID b)
-    {
-        assert(a == Arg::TypeID::__ISARRAY || b == Arg::TypeID::__ISARRAY);
-        return static_cast<Arg::TypeID>(int(a) & int(b));
-    }
 
     struct EventAttributes {
     private:
@@ -276,56 +210,6 @@ namespace internal {
         virtual void sendSample_(const Arg values[]) = 0;
     };
 
-namespace {
-    template<class T> struct TypeIDGetter {
-        static constexpr Arg::TypeID value = Arg::TypeID::TI_UNKNOWN;
-    };
-    template<> struct TypeIDGetter<char>     { static constexpr Arg::TypeID value = Arg::TypeID::TI_Char; };
-    template<> struct TypeIDGetter<float>    { static constexpr Arg::TypeID value = Arg::TypeID::TI_f32; };
-    template<> struct TypeIDGetter<double>   { static constexpr Arg::TypeID value = Arg::TypeID::TI_f64; };
-    template<> struct TypeIDGetter<uint8_t>  { static constexpr Arg::TypeID value = Arg::TypeID::TI_u8; };
-    template<> struct TypeIDGetter<uint16_t> { static constexpr Arg::TypeID value = Arg::TypeID::TI_u16; };
-    template<> struct TypeIDGetter<uint32_t> { static constexpr Arg::TypeID value = Arg::TypeID::TI_u32; };
-    template<> struct TypeIDGetter<uint64_t> { static constexpr Arg::TypeID value = Arg::TypeID::TI_u64; };
-    template<> struct TypeIDGetter<int8_t>   { static constexpr Arg::TypeID value = Arg::TypeID::TI_i8; };
-    template<> struct TypeIDGetter<int16_t>  { static constexpr Arg::TypeID value = Arg::TypeID::TI_i16; };
-    template<> struct TypeIDGetter<int32_t>  { static constexpr Arg::TypeID value = Arg::TypeID::TI_i32; };
-    template<> struct TypeIDGetter<int64_t>  { static constexpr Arg::TypeID value = Arg::TypeID::TI_i64; };
-
-    inline void logEvent_sfinae(Arg * arg, std::thread::id && a) {
-        arg->type = Arg::TypeID::TI_Thread;
-        std::memcpy(&arg->valueOrArray.Thread, &a, sizeof(a));
-    }
-    inline void logEvent_sfinae_pchar(Arg * arg, const char * p) {
-        arg->type = static_cast<Arg::TypeID>((int)TypeIDGetter<char>::value | (int)Arg::TypeID::__ISARRAY);
-        arg->valueOrArray.ArrayPointer = p;
-        arg->arrayLength = std::strlen(p);
-    }
-    inline void logEvent_sfinae(Arg * arg, const char * p) {
-        logEvent_sfinae_pchar(arg, p);
-    }
-    inline void logEvent_sfinae(Arg * arg, char * p) {
-        logEvent_sfinae_pchar(arg, p);
-    }
-    template<class C, class D> inline void logEvent_sfinae(Arg * arg, std::chrono::time_point<C,D> && a) {
-        arg->type = Arg::TypeID::TI_EpochNsec;
-        arg->valueOrArray.EpochNsec = a.time_since_epoch().count();
-    }
-    template<size_t N> inline void logEvent_sfinae(Arg * arg, const char (&p)[N]) {
-        arg->type = static_cast<Arg::TypeID>((int)TypeIDGetter<char>::value | (int)Arg::TypeID::__ISARRAY);
-        arg->valueOrArray.ArrayPointer = &p[0];
-        arg->arrayLength = N-1;
-    }
-    template<class T> inline void logEvent_sfinae(Arg * arg, T && a) {
-        arg->type = TypeIDGetter<T>::value;
-        std::memcpy(&arg->valueOrArray, &a, sizeof(T));
-    }
-    template<class T, class...Ts> inline void logEvent_sfinae(Arg * argBuf, T && fst, Ts &&... args) {
-        logEvent_sfinae(argBuf++, std::forward<T&&>(fst));
-        logEvent_sfinae(argBuf, std::forward<Ts&&>(args)...);
-    }
-    inline void logEvent_sfinae(Arg * argBuf) { argBuf->type = Arg::TypeID::TI_NONE; }
-}
     template<typename TEventChannel>
     inline const EventID registerEvent(TEventChannel & channel, const EventAttributes & attr)
     {
@@ -339,7 +223,7 @@ namespace {
     inline void logEvent(TEventChannel & channel, const EventAttributes & attributes, Ts &&... args)
     {
         Arg * argBuf = channel.AbstractEventChannel::allocateArgs(sizeof...(Ts));
-        logEvent_sfinae(argBuf, std::forward<Ts&&>(args)...);
+        _utl::logging::internal::fillArgsBuffer(argBuf, std::forward<Ts&&>(args)...);
         channel.AbstractEventChannel::sendEventOccurrence(attributes, argBuf);
     }
 
